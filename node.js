a97ae1539acfe02461e4be37f6a6f4c58f724434
Anas Bouzanbil <abouzanb@c3r10p1.1337.ma>
2023-09-17 12:37:12 +0100

    Handling some errors

--- node.js
+++ node.js
@@ -1,42 +1,3 @@
-// var fs = require('fs');
-
-
-// var s = fs.readFileSync('n.py', 'utf8');
-// var q = fs.writeFileSync('testi.py', s);
-// console.log(s);
-
-// var s = require('http');
-
-// var d = require('fs');
-
-// //server 
-
-
-
-// var t = "/nfs/homes/abouzanb/Downloads"
-// var server = s.createServer(function (req, res){
-//     var m = {
-//         name: "anas",
-//         age: 445,
-//         again: "oooo"
-//     }
-//     try {
-//         // var a = d.readFileSync(t + req.url);
-		
-//         res.writeHead(200, {'Content-Type': 'application/json'});
-//         res.end(JSON.stringify(m));
-//         // var streams = d.createReadStream('/nfs/homes/abouzanb/Downloads/oulad.mp4');
-//         // streams.pipe(res);
-//     }
-//     catch(error)
-//     {
-//         var a = d.readFileSync('index.html');
-//         res.writeHead(404, {'Content-type' : 'text/html'});
-//         res.end(a);
-//     }    
-// });
-// server.listen(8050, '10.13.10.5');
-// console.log("it is like cpp");
 
 var serv = require('http');
 var fi = require('fs');
@@ -72,7 +33,7 @@ var s = serv.createServer((function(req, res){
 		if (err)
 		{
 			res.writeHead(404, {'Content-Type' : 'text/html'});
-			fi.createReadStream("/nfs/homes/abouzanb/Desktop/chat/server_one/errors/error404.html").pipe(res);
+			fi.createReadStream("./server_one/errors/error403.html").pipe(res);
 			return ;
 		}
 		if (stats.isDirectory())
@@ -83,22 +44,11 @@ var s = serv.createServer((function(req, res){
 		if (stats.isFile())
 		{
 			var s = map_extansion[paths.extname(req.url)];
-			if (req.url === '/login')
-			{
-				res.writeHead(200, {'Content-Type' : s});
-				fi.createReadStream("./server_one/login.html").pipe(res);        
-			}
-			if (req.url === '/signup')
-			{
-				res.writeHead(200, {'Content-Type' : s});
-				fi.createReadStream("./server_one/signup.html").pipe(res);
-		
-			}
-			else
-			{
+			
+			
 				res.writeHead(200, {'Content-Type' : s});
 				fi.createReadStream("./server_one" + req.url).pipe(res);
-			}
+			
 			res.end();
 		}
 
